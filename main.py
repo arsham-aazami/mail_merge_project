6f79cc7ccca943caad770407b7b345060dfa844c
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-11-21 23:16:49 +0330

    last updated

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ with open("./Input/Names/invited_names.txt") as names:
 with open("./Input/Letters/starting_letter.txt") as letter:
     read_letter = letter.read()
     for name in names_list:
-        new_letter = read_letter.replace(PLACEHOLDER, name)
+        striped_name = name.strip()
+        new_letter = read_letter.replace(PLACEHOLDER, striped_name)
         print(new_letter)
-        with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as invitation:
+        with open(f"./Output/ReadyToSend/letter_for_{striped_name}.txt", mode="w") as invitation:
             invitation.write(new_letter)
-        # with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as invitation:
-        # 	invitation.write(new_letter)
+
 
